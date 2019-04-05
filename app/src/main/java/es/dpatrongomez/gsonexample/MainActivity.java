5402520e32909c2465c49c5168176b87c6df0e52
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2019-04-05 13:31:42 +0200

    Create all with Serialization

--- app/src/main/java/es/dpatrongomez/gsonexample/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/gsonexample/MainActivity.java
@@ -3,11 +3,22 @@ package es.dpatrongomez.gsonexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+        /*
+        Employee employee = new Employee("Daniel", 20, "dpatrongomez@gmail.com");
+        String json = gson.toJson(employee);
+        */
+
+        String json = "{\"first_name\":\"Daniel\",\"age\":20,\"mail\":\"dpatrongomez@gmail.com\"}";
+        Employee employee = gson.fromJson(json, Employee.class);
     }
 }
